8c96cd4d7a9dff371a392463cb786eda09701c4c
James Michelson <Jamie@Jamess-MacBook-Air.local>
2015-09-18 10:37:35 -0400

    4->5

---
+++ bandit/bandit.sh
@@ -0,0 +1,27 @@
+# Notes:
+# http://overthewire.org/wargames/bandit/
+
+# 0
+cat readme
+# boJ9jbbUNNfktd78OOpsqOltutMc3MY1i
+
+# 1
+cat ./-
+# CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9
+
+# 2 
+cat spaces\ in\ this\ filename
+# UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
+
+# 3
+cd inhere
+ls -la
+cat .hidden
+# pIwrPrtPN36QITSp3EQaw936yaFoFgAB
+
+# 4 
+find ./inhere | xargs file | grep text 
+cd inhere && cat ./-file07
+# koReBOKuIDDepwhWk7jZC0RTdopnAYKh
+
+
