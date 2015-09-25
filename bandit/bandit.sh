ff64780af1b9f9f6d4443f2becd8118f422398b1
James Michelson <Jamie@Jamess-MacBook-Air.local>
2015-09-24 23:28:09 -0400

    problem 6

--- bandit/bandit.sh
+++ bandit/bandit.sh
@@ -1,27 +1,29 @@
 # Notes:
 # http://overthewire.org/wargames/bandit/
 
-# 0
+# 1
 cat readme
 # boJ9jbbUNNfktd78OOpsqOltutMc3MY1i
 
-# 1
+# 2
 cat ./-
 # CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9
 
-# 2 
+# 3 
 cat spaces\ in\ this\ filename
 # UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
 
-# 3
+# 4
 cd inhere
 ls -la
 cat .hidden
 # pIwrPrtPN36QITSp3EQaw936yaFoFgAB
 
-# 4 
+# 5 
 find ./inhere | xargs file | grep text 
 cd inhere && cat ./-file07
 # koReBOKuIDDepwhWk7jZC0RTdopnAYKh
 
-
+# 6
+find ./inhere | xargs file | grep text | cut -d \t -f 1 | sed -e 's/: ASCII//g' | xargs du -b | grep 1033 | xargs cat
+# DXjZPULLxYr17uwoI01bNLQbtFemEgo7
